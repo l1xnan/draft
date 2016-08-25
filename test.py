0dd0fbc865506b86d15e3eb13de03892c41fd5c2
l1xnan <lixnan.g@gmail.com>
2016-08-25 19:47:57 +0800

    mongo group

--- test.py
+++ test.py
@@ -1,6 +1,44 @@
-tags = []
-
-# with open('tags.txt', 'r', encoding='utf-8') as file:
-#     for line in file:
-#         tags.append(line.strip())
-print(tags)
+def score_staging_class(course):
+    condition = [
+        {"$match": {"score." + course + ".state": 0}},
+        {'$group': {
+            '_id': {"class_id": "$class_id"},
+            'avg': {'$avg': "$score." + course + ".final"},
+            'total': {'$sum': 1},
+            's1': {'$sum': {'$cond': [{
+                '$and': [
+                    {"$lt": ["$score." + course + ".final", {
+                        '$multiply': ["$score." + course + ".full", 0.6]}]},
+                    {"$gte": ["$score." + course + ".final", {
+                        '$multiply': ["$score." + course + ".full", 0]}]}
+                ]
+            }, 1, 0]}},
+            's2': {'$sum': {'$cond': [{
+                '$and': [
+                    {"$lt": ["$score." + course + ".final", {
+                            '$multiply': ["$score." + course + ".full", 0.8]}]},
+                    {"$gte": ["$score." + course + ".final", {
+                        '$multiply': ["$score." + course + ".full", 0.6]}]}
+                ]
+            }, 1, 0]}},
+            's3': {'$sum': {'$cond': [{
+                '$and': [
+                    {"$lt": ["$score." + course + ".final", {
+                            '$multiply': ["$score." + course + ".full", 0.9]}]},
+                    {"$gte": ["$score." + course + ".final", {
+                        '$multiply': ["$score." + course + ".full", 0.8]}]}
+                ]
+            }, 1, 0]}},
+            's4': {'$sum': {'$cond': [{
+                '$and': [
+                    {"$lte": ["$score." + course + ".final", {
+                            '$multiply': ["$score." + course + ".full", 1.0]}]},
+                    {"$gte": ["$score." + course + ".final", {
+                        '$multiply': ["$score." + course + ".full", 0.9]}]}
+                ]
+            }, 1, 0]}},
+        }
+        },
+        {"$sort": {"_id": 1}}]
+#     result = db.finalexam.aggregate(condition)
+    return db.finalexam.aggregate(condition)
